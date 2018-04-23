4edb26f8150614788856c43f429f82b0aaeebcdd
broodingmonk98 <es16btech11014@iith.ac.in>
2018-04-23 11:02:14 +0530

    Initialization

--- code.cpp
+++ code.cpp
@@ -14,44 +14,44 @@ typedef struct header{
    //ends at 1048
 } header;
 
-header *hash_bucket[12];
+header *hash_bucket[12]={NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
 void* mymalloc(unsigned size);
 void myfree(void *ptr);
 
-int mask[32];
+int mask[32] = {0x80000000,0x40000000,0x20000000,0x10000000,0x08000000,0x04000000,0x02000000,0x01000000,0x00800000,0x00400000,0x00200000,0x00100000,0x00080000,0x00040000,0x00020000,0x00010000,0x00008000,0x00004000,0x00002000,0x00001000,0x00000800,0x00000400,0x00000200,0x00000100,0x00000080,0x00000040,0x00000020,0x00000010,0x00000008,0x00000004,0x00000002,0x00000001};
 
-int main()
-{
-    printf("Initializing\n");
-    //initialize
-    for(int i=0;i<12;i++)
-        hash_bucket[i] = NULL;
-    for(int i=31,j=1;i>=0;i--,j<<=1)
-        mask[i]=j;
-    //done initializing
-    int *ptr[5500];
-    printf("starting off\n");
-    for(int i =0 ;i<5500;i++){
-        ptr[i] = (int*)mymalloc(100);
-        if(i%100==0)
-            printf("%d\n",i);
-
-    }
-    printf("FREEING\n");
-    for(int i =0 ;i<5500;i++){
-
-        printf("%d  ",i);
-        myfree((void*)ptr[i]);
-    }
-
-    //int *ptr = (int*)mymalloc(10);
-    //ptr = (int*)mymalloc(10);
-    //printf("%ld",ptr-ptr1);
-    printf("Done\n");
-
-
-    return 0;
-}
+//int main()
+//{
+//    printf("Initializing\n");
+//    //initialize
+//    for(int i=0;i<12;i++)
+//        hash_bucket[i] = NULL;
+//    for(int i=31,j=1;i>=0;i--,j<<=1)
+//        mask[i]=j;
+//    //done initializing
+//    int *ptr[5500];
+//    printf("starting off\n");
+//    for(int i =0 ;i<5500;i++){
+//        ptr[i] = (int*)mymalloc(100);
+//        if(i%100==0)
+//            printf("%d\n",i);
+//
+//    }
+//    printf("FREEING\n");
+//    for(int i =0 ;i<5500;i++){
+//
+//        printf("%d  ",i);
+//        myfree((void*)ptr[i]);
+//    }
+//
+//    //int *ptr = (int*)mymalloc(10);
+//    //ptr = (int*)mymalloc(10);
+//    //printf("%ld",ptr-ptr1);
+//    printf("Done\n");
+//
+//
+//    return 0;
+//}
 
 void* mymalloc(unsigned size){
     int i=0;
